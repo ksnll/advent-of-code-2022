a15a7dc802565e88222c3596cb2753178e74e543
Federico Blancato <federico.blancato@infinitaslearning.com>
2022-12-03 11:08:33 +0100

    Day 3 part 1

---
+++ src/bin/03.rs
@@ -0,0 +1,38 @@
+use std::{error::Error, fs};
+
+fn get_priority(s1: &str, s2: &str) -> Option<u32> {
+    for c1 in s1.chars() {
+        for c2 in s2.chars() {
+            if c1 == c2 {
+                if c1 > 'a' {
+                    return Some(c1 as u8 as u32 - 96_u32);
+                }
+                if c1 > 'A' {
+                    return Some(c1 as u8 as u32 - 38_u32);
+                }
+            }
+        }
+    }
+    None
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("./inputs/03.txt")?;
+    let mut priorities = Vec::new();
+    for line in input.lines() {
+        let (first_half, second_half) = line.split_at(line.len() / 2);
+        match get_priority(first_half, second_half) {
+            Some(val) => priorities.push(val),
+            None => panic!("Couldn't find same item in rucksack"),
+        }
+    }
+    dbg!(priorities.into_iter().sum::<u32>());
+    Ok(())
+}
+
+#[cfg(test)]
+#[test]
+fn it_should_find_rucksack_priority() {
+    assert_eq!(Some(16), get_priority("vJrwpWtwJgWr", "hcsFMMfFFhFp"));
+    assert_eq!(Some(42), get_priority("PmmdzqPrV", "vPwwTWBwg"));
+}
